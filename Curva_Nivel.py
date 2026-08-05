a87cdc18e31ed274b531e4bacf96b7a03dc56cd2
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-05 15:20:38 -0300

    Definir a função z = f(x, y).

--- Curva_Nivel.py
+++ Curva_Nivel.py
@@ -7,3 +7,7 @@ y = np.linspace(-5, 5, 200)
 
 # 2. Criar a grade bidimensional (Meshgrid)
 X, Y = np.meshgrid(x, y)
+
+# 3. Definir a função z = f(x, y)
+# Exemplo: z = sin(sqrt(x^2 + y^2))
+Z = np.sin(np.sqrt(X**2 + Y**2))
