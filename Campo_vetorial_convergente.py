a4dea43354c1e1ae1e527f3e050c242d031073e3
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-07 13:53:56 -0300

    Criar a grade de pontos (X, Y) no plano cartesiano.

--- Campo_vetorial_convergente.py
+++ Campo_vetorial_convergente.py
@@ -1,2 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# 1. Criar a grade de pontos (X, Y) no plano cartesiano
+x = np.linspace(-5, 5, 20)
+y = np.linspace(-5, 5, 20)
+X, Y = np.meshgrid(x, y)
