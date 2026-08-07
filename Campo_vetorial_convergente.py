cdf5ce6a97c997728d3271ba96b49098c2f095b7
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-07 13:54:13 -0300

    Definir as componentes do vetor para um campo convergente.

--- Campo_vetorial_convergente.py
+++ Campo_vetorial_convergente.py
@@ -5,3 +5,8 @@ import matplotlib.pyplot as plt
 x = np.linspace(-5, 5, 20)
 y = np.linspace(-5, 5, 20)
 X, Y = np.meshgrid(x, y)
+
+# 2. Definir as componentes do vetor para um campo convergente
+# Para convergir para a origem, as componentes devem ser proporcionais a -X e -Y
+U = -X
+V = -Y
