e4b155da6ee9f3488b1cef85a1b7b7d23db09602
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-08 19:14:11 -0300

    Definindo as componentes do campo vetorial rotacional

--- Campo_Vetorial_Rotacional.py
+++ Campo_Vetorial_Rotacional.py
@@ -5,3 +5,8 @@ import matplotlib.pyplot as plt
 x = np.linspace(-5, 5, 20) # Cria 20 pontos de -5 a 5 no eixo X
 y = np.linspace(-5, 5, 20) # Cria 20 pontos de -5 a 5 no eixo Y
 X, Y = np.meshgrid(x, y)   # Cria uma malha 2D com esses pontos
+
+# 2. Definindo as componentes do campo vetorial rotacional
+# V(x, y) = -y*i + x*j
+U = -Y  # Componente X do vetor
+V = X   # Componente Y do vetor
