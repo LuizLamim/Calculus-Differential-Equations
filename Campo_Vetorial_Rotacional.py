6489c82aada40f90f96219ac673cf52eea15e1db
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-08 19:13:49 -0300

    Definindo o espaço (grid)

--- Campo_Vetorial_Rotacional.py
+++ Campo_Vetorial_Rotacional.py
@@ -1,2 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# 1. Definindo o espaço (grid)
+x = np.linspace(-5, 5, 20) # Cria 20 pontos de -5 a 5 no eixo X
+y = np.linspace(-5, 5, 20) # Cria 20 pontos de -5 a 5 no eixo Y
+X, Y = np.meshgrid(x, y)   # Cria uma malha 2D com esses pontos
