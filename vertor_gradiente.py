f3e67c9712c0fb72b5ab5f26d04dd0d2a50fcc2f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-10 20:00:30 -0300

    Definir a função escalar f(x, y)

--- vertor_gradiente.py
+++ vertor_gradiente.py
@@ -5,3 +5,7 @@ import numpy as np
 x = np.linspace(-3, 3, 20)
 y = np.linspace(-3, 3, 20)
 X, Y = np.meshgrid(x, y)
+
+# 2. Definir a função escalar f(x, y)
+# Exemplo: f(x, y) = x² + y² (um paraboloide)
+Z = X**2 + Y**2
