926dae9f24ea8fc7fc52b506b6621ec93ecd776e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-10 20:00:15 -0300

    Criar a grade de pontos no plano 2D (x, y)

--- vertor_gradiente.py
+++ vertor_gradiente.py
@@ -1,2 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+# 1. Criar a grade de pontos no plano 2D (x, y)
+x = np.linspace(-3, 3, 20)
+y = np.linspace(-3, 3, 20)
+X, Y = np.meshgrid(x, y)
