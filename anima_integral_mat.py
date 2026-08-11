25a175c40cb5e39915213851f1a0d1c861d2d9ed
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-11 20:43:07 -0300

    Definição da função e do intervalo.

--- anima_integral_mat.py
+++ anima_integral_mat.py
@@ -1,3 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
+# 1. Definição da função e do intervalo
+def f(x):
+    # Função para o exemplo: f(x) = x^2 + 1
+    return x**2 + 1
+
+a, b = 0, 2  # Intervalo [a, b]
