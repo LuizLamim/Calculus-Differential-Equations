3cb4241f5eebbf2a475f3a35eb9d8b4d9a766170
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-12 18:45:50 -0300

    Calcula os valores da função f(x) = e^x + sin(x)

--- f(x)=e^x+sin(x).py
+++ f(x)=e^x+sin(x).py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 
 # 1. Define o intervalo de valores para x (de -3 a 3 com 500 pontos)
 x = np.linspace(-3, 3, 500)
+
+# 2. Calcula os valores da função f(x) = e^x + sin(x)
+y = np.exp(x) + np.sin(x)
