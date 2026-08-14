c4dc4b57d06cfb439b8ef43748e45757c17e085f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-14 19:11:52 -0300

    ariável formatada para exibir o resultado.

--- soma_2.cbl
+++ soma_2.cbl
@@ -9,3 +9,6 @@
        01  WS-NUM1        PIC 9(05) VALUE ZEROS.
        01  WS-NUM2        PIC 9(05) VALUE ZEROS.
        01  WS-RESULTADO   PIC 9(06) VALUE ZEROS.
+
+       *> Variável formatada para exibir o resultado sem zeros à esquerda
+       01  WS-RESULT-EDIT PIC ZZZ,ZZ9.
