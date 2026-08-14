5b859a6f4630c5b9838f1916a50f2ed799fdbee5
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-13 22:08:26 -0300

    3

--- cos_x_35.py
+++ cos_x_35.py
@@ -3,3 +3,6 @@ import numpy as np
 
 # 1. Define o intervalo de valores para x (ex: de -2π a 2π)
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+
+# 2. Calcula a função y = cos(x) + 35
+y = np.cos(x) + 35
