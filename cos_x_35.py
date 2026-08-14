ebbb75a1520a90586a8191e793e84149798afee5
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-13 22:08:36 -0300

    4

--- cos_x_35.py
+++ cos_x_35.py
@@ -6,3 +6,7 @@ x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
 
 # 2. Calcula a função y = cos(x) + 35
 y = np.cos(x) + 35
+
+# 3. Configura a figura do gráfico
+plt.figure(figsize=(10, 5))
+plt.plot(x, y, label=r"$f(x) = \cos(x) + 35$", color="indigo", linewidth=2)
