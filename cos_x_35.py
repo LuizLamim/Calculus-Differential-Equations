924f9f001559c16a773eb216240afc06d280fc4b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-13 22:08:47 -0300

    4

--- cos_x_35.py
+++ cos_x_35.py
@@ -10,3 +10,18 @@ y = np.cos(x) + 35
 # 3. Configura a figura do gráfico
 plt.figure(figsize=(10, 5))
 plt.plot(x, y, label=r"$f(x) = \cos(x) + 35$", color="indigo", linewidth=2)
+
+# 4. Personalização das marcas do eixo X em termos de Pi (π)
+ticks_x = np.array([-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]) * np.pi
+labels_x = [
+    r"$-2\pi$",
+    r"$-3\pi/2$",
+    r"$-\pi$",
+    r"$-\pi/2$",
+    "$0$",
+    r"$\pi/2$",
+    r"$\pi$",
+    r"$3\pi/2$",
+    r"$2\pi$",
+]
+plt.xticks(ticks_x, labels_x)
