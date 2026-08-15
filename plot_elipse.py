5ef48e8dcd52b51b1aca7806181fd1fa0a52030e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-15 20:34:21 -0300

    Configuração do gráfico.

--- plot_elipse.py
+++ plot_elipse.py
@@ -12,3 +12,8 @@ t = np.linspace(0, 2 * np.pi, 500)
 # Equações paramétricas da elipse
 x = h + a * np.cos(t)
 y = k + b * np.sin(t)
+
+# Configuração do gráfico
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, label=f'Elipse: a={a}, b={b}', color='blue', linewidth=2)
+plt.plot(h, k, 'ro', label='Centro (0,0)')  # Plota o ponto do centro
