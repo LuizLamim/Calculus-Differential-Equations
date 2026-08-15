7ed72594753b51450f18445db6c79a80d4e8ad2b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-15 20:33:35 -0300

    Parâmetros da elipse

--- plot_elipse.py
+++ plot_elipse.py
@@ -1,2 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Parâmetros da elipse
+h, k = 0, 0  # Centro da elipse (h, k)
+a = 5        # Semieixo maior (horizontal)
+b = 3        # Semieixo menor (vertical)
