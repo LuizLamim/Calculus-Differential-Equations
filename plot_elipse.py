90f68cf2cfc3a00a8bda5f99e4d2c7c77d6ccf9e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-15 20:34:04 -0300

    Ângulo t variando de 0 a 2π e Equações paramétricas da elipse.

--- plot_elipse.py
+++ plot_elipse.py
@@ -5,3 +5,10 @@ import numpy as np
 h, k = 0, 0  # Centro da elipse (h, k)
 a = 5        # Semieixo maior (horizontal)
 b = 3        # Semieixo menor (vertical)
+
+# Ângulo t variando de 0 a 2π
+t = np.linspace(0, 2 * np.pi, 500)
+
+# Equações paramétricas da elipse
+x = h + a * np.cos(t)
+y = k + b * np.sin(t)
