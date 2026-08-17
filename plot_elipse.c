767334cfb8a42ea50434676e4e95a9d66998166f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-17 18:56:35 -0300

    Tela de caracteres e Parâmetros da elipse

--- plot_elipse.c
+++ plot_elipse.c
@@ -5,5 +5,16 @@
 #define ALTURA 30
 
 int main{
+    // Tela de caracteres
+    char tela[ALTURA][LARGURA];
+
+    // Parâmetros da elipse
+    double a = 20.0; // Raio horizontal (em caracteres)
+    double b = 8.0;  // Raio vertical (em linhas)
+
+    // Centro da tela
+    int centroX = LARGURA / 2;
+    int centroY = ALTURA / 2;
+    
     return 0;
 }
