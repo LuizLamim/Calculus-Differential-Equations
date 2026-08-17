b6bb91997a5978f65c1de81c362138e58bbe1d58
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-16 22:35:12 -0300

    a = 8

--- plot_elipse.py
+++ plot_elipse.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Parâmetros da elipse
 h, k = 0, 0  # Centro da elipse (h, k)
-a = 5        # Semieixo maior (horizontal)
+a = 8        # Semieixo maior (horizontal)
 b = 3        # Semieixo menor (vertical)
 
 # Ângulo t variando de 0 a 2π
