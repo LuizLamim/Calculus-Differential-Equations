3a50a7a50d8adb1e02ce5c6776cb07f16b69db8b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-18 19:52:37 -0300

    Calcular o Vetor Gradiente (df/dx, df/dy)

--- vetor_gradiente.py
+++ vetor_gradiente.py
@@ -9,3 +9,8 @@ X, Y = np.meshgrid(x, y)
 # 2. Definir a função escalar f(x, y)
 # Exemplo: f(x, y) = x^2 + y^2
 Z = X**2 + Y**2
+
+# 3. Calcular o Vetor Gradiente (df/dx, df/dy)
+# O gradiente analítico de x^2 + y^2 é (2x, 2y)
+# Também podemos usar np.gradient para calcular numericamente:
+U, V = np.gradient(Z, x[1] - x[0], y[1] - y[0])
