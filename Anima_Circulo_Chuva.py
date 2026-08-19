4177c80c6d065cf46d2ae377b1508eb4d5f042cc
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-19 20:42:31 -0300

    Configurações da Tela

--- Anima_Circulo_Chuva.py
+++ Anima_Circulo_Chuva.py
@@ -3,3 +3,9 @@ import random
 
 # Inicializa o Pygame
 pygame.init()
+
+# Configurações da Tela
+LARGURA = 800
+ALTURA = 600
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Chuva de Círculos")
