6d02892fb222de80b98df130868fdf7f29f93ca4
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-21 19:08:47 -0300

    Inicializa a grade com espaços em branco

--- plot_x_2.c
+++ plot_x_2.c
@@ -5,4 +5,11 @@
 
 int main() {
     char grade[ALTURA][LARGURA];
+
+    // 1. Inicializa a grade com espaços em branco
+    for (int i = 0; i < ALTURA; i++) {
+        for (int j = 0; j < LARGURA; j++) {
+            grade[i][j] = ' ';
+        }
+    }
 }
