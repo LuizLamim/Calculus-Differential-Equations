37c1cd70e851da3b06010a1a0c3689de2f3403dc
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-22 19:23:46 -0300

    Define o intervalo para as coordenadas x e y de -10 a 10

--- Plot_Plano_Area.py
+++ Plot_Plano_Area.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 # 1. Cria a figura e o eixo 3D
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
+
+# 2. Define o intervalo para as coordenadas x e y (de -10 a 10)
+x = np.linspace(-10, 10, 20)
+y = np.linspace(-10, 10, 20)
+X, Y = np.meshgrid(x, y)
