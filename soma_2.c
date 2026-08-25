b4b71aa1dbdeaf97476c68e5557ec7f7dd69413f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-24 23:40:05 -0300

    Leitura dos dados de entrada

--- soma_2.c
+++ soma_2.c
@@ -3,3 +3,10 @@
 int main()
 
     float num1, num2, soma;
+
+    // Leitura dos dados de entrada
+    printf("Digite o primeiro número: ");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%f", &num2);
