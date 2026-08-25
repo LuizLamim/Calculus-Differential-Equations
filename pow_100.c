d320adc9e604e0df33d7763e73a3f54921746235
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-25 17:35:49 -0300

    Pow 100

--- pow_100.c
+++ pow_100.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
     double base, resultado;
 
-    return 0;
+    printf("Digite o numero base: ");
+    if (scanf("%lf", &base) != 1) {
+        printf("Entrada invalida!\n");
+        return 1;
+    }
 
+    // Calcula base^100
+    resultado = pow(base, 100);
+
+    // Exibe em notacao cientifica e decimal padrao
+    printf("\nResultado de %.2lf elevado a 100:\n", base);
+    printf("Notacao Cientifica: %e\n", resultado);
+    printf("Notacao Decimal:    %.2f\n", resultado);
+
+    return 0;
 }
