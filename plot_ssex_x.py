2b9a6b2bd7d5b405b31b18c961faf71cf4d37f79
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-27 20:44:20 -0300

    Calculando a secante e aplicando o valor absoluto |sec(x)|

--- plot_ssex_x.py
+++ plot_ssex_x.py
@@ -4,3 +4,8 @@ import numpy as np
 # Definindo o intervalo de x (ex: de -2π a 2π)
 x = np.linspace(-2 * np.pi, 2 * np.pi, 2000)
 
+# Calculando a secante e aplicando o valor absoluto |sec(x)|
+y = np.abs(1 / np.cos(x))
+
+# Removendo descontinuidades (assíntotas) para não desenhar linhas verticais falsas
+y[y > 10] = np.nan
