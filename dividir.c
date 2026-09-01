af948e782d66e94b1ef77eec888d5460e912118f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-01 20:21:06 -0300

    5

--- dividir.c
+++ dividir.c
@@ -2,4 +2,19 @@
 
 int main(){
     double num1, num2, resultado;
+
+    // Entrada dos números
+    printf("Digite o primeiro número (dividendo): ");
+    scanf("%lf", &num1);
+
+    printf("Digite o segundo número (divisor): ");
+    scanf("%lf", &num2);
+
+    // Verificação essencial para evitar divisão por zero
+    if (num2 == 0) {
+        printf("Erro: Não é possível realizar divisão por zero!\n");
+    } else {
+        resultado = num1 / num2;
+        printf("O resultado de %.2lf / %.2lf é: %.2lf\n", num1, num2, resultado);
+    }
 }
