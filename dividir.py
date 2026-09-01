b66a26cc27522c57c010b9e5438650bee601a775
LuizLamim <luiz.britohenrique@hotmail.com>
2026-08-31 21:45:23 -0300

    print

--- dividir.py
+++ dividir.py
@@ -10,3 +10,4 @@ num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
 resultado = dividir(num1, num2)
+print(f"O resultado da divisão é: {resultado}")
