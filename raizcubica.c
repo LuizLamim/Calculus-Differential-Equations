0fc509f72ab225e5c8b58b0cea6844cbc595f910
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-03 19:47:50 -0300

    6

--- raizcubica.c
+++ raizcubica.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <math.h> // Necessário para a função cbrt()
 
-int main(){
+int main() {
     double numero, resultado;
+
+    // Solicita o número ao usuário
+    printf("Digite um numero para calcular a raiz cubica: ");
+    if (scanf("%lf", &numero) == 1) {
+        
+        // Calcula a raiz cúbica
+        resultado = cbrt(numero);
+
+        // Exibe o resultado formatado
+        printf("A raiz cubica de %.2lf e %.4lf\n", numero, resultado);
+        
+    } else {
+        printf("Por favor, insira um numero valido.\n");
+    }
+
+    return 0;
 }
