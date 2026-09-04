5902d9556b7ac77d7e34e19b8c7ed30637e33e6c
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-04 18:19:27 -0300

    Definindo as funções

--- deriva_x2_2x.py
+++ deriva_x2_2x.py
@@ -3,3 +3,7 @@ import numpy as np
 
 # 1. Gerando os valores de x (de -10 até 10 com 400 pontos para suavizar a curva)
 x = np.linspace(-10, 10, 400)
+
+# 2. Definindo as funções
+y_quadratica = x**2
+y_linear = 2 * x
