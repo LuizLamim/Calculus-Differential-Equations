b0051140830f585dc2cacc3dd8949083a6efb774
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-05 20:55:42 -0300

    Laço FOR

--- Neural_Network_Color.py
+++ Neural_Network_Color.py
@@ -10,3 +10,14 @@ def gerar_dados_treino(quantidade):
     # Gera 'quantidade' de cores RGB (valores de 0 a 255)
     X = np.random.randint(0, 256, size=(quantidade, 3))
     y = []
+
+    for r, g, b in X:
+        # Fórmula de luminância (como o olho humano percebe o brilho)
+        luminancia = (0.299 * r + 0.587 * g + 0.114 * b) / 255.0
+        
+        if luminancia > 0.5:
+            y.append(0) # Fundo claro -> Texto Preto
+        else:
+            y.append(1) # Fundo escuro -> Texto Branco
+            
+    return X, np.array(y)
