cf68b2772bb2b6a7b9b4a478458e4f3d51a4b93c
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-05 20:55:21 -0300

    PREPARAÇÃO DOS DADOS

--- Neural_Network_Color.py
+++ Neural_Network_Color.py
@@ -3,3 +3,10 @@ from sklearn.neural_network import MLPClassifier
 
 # 1. PREPARAÇÃO DOS DADOS
 def gerar_dados_treino(quantidade):
+    """
+    Gera cores RGB aleatórias e define a resposta correta (0 para Preto, 1 para Branco)
+    com base na fórmula de luminância perceptiva humana.
+    """
+    # Gera 'quantidade' de cores RGB (valores de 0 a 255)
+    X = np.random.randint(0, 256, size=(quantidade, 3))
+    y = []
