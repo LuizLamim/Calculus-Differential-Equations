b87c8471aa8c58b19178c36eb7543d5692aab7ee
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-08 20:10:32 -0300

    6

--- exp.c
+++ exp.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
     double base, expoente, resultado;
+
+    // Solicita os dados ao usuário
+    printf("Digite a base: ");
+    scanf("%lf", &base);
+
+    printf("Digite o expoente: ");
+    scanf("%lf", &expoente);
+
+    // Calcula a potência (base elevada ao expoente)
+    resultado = pow(base, expoente);
+
+    // Exibe o resultado
+    printf("%.2lf elevado a %.2lf e igual a: %.2lf\n", base, expoente, resultado);
 
     return 0;
 }
