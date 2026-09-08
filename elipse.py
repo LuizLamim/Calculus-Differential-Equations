cea83d52db860800dc9b7ef9f097441b99bac41d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-07 21:23:06 -0300

    Configuração para formato vertical (9:16)

--- elipse.py
+++ elipse.py
@@ -1,2 +1,8 @@
 from manim import *
 import numpy as np
+
+# Configuração para formato vertical (9:16)
+config.pixel_width = 1080
+config.pixel_height = 1920
+config.frame_width = 9
+config.frame_height = 16
