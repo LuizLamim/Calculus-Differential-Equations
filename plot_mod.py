1edab77805ae539c45c7d3ae3b9b6413be956d22
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-11 18:47:03 -0300

    5

--- plot_mod.py
+++ plot_mod.py
@@ -14,3 +14,11 @@ plt.figure(figsize=(10, 5))
 # Plotagem das curvas
 plt.plot(x, y_cos, label=r'$|\cos(x)|$', color='crimson', linewidth=2)
 plt.plot(x, y_sin, label=r'$|\sin(x)|$', color='dodgerblue', linewidth=2)
+
+# Formatação dos rótulos do eixo X em múltiplos de π/2
+xticks = np.arange(-2 * np.pi, 2.5 * np.pi, np.pi / 2)
+xtick_labels = [
+    r'$-2\pi$', r'$-\frac{3\pi}{2}$', r'$-\pi$', r'$-\frac{\pi}{2}$',
+    '0', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'
+]
+plt.xticks(xticks, xtick_labels)
