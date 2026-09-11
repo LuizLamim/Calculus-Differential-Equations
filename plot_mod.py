22d0091b05f46a22f9db52eff58679ca82abf7b8
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-11 18:46:54 -0300

    4

--- plot_mod.py
+++ plot_mod.py
@@ -7,3 +7,10 @@ x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
 # Cálculo do módulo das funções trigonométricas
 y_cos = np.abs(np.cos(x))
 y_sin = np.abs(np.sin(x))
+
+# Criação da figura
+plt.figure(figsize=(10, 5))
+
+# Plotagem das curvas
+plt.plot(x, y_cos, label=r'$|\cos(x)|$', color='crimson', linewidth=2)
+plt.plot(x, y_sin, label=r'$|\sin(x)|$', color='dodgerblue', linewidth=2)
