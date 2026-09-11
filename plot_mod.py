ddac2a4fb7ba10d633f37a00ead4bd1754f3dc6d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-11 18:46:41 -0300

    3

--- plot_mod.py
+++ plot_mod.py
@@ -3,3 +3,7 @@ import numpy as np
 
 # Configuração do domínio x de -2π a 2π
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+
+# Cálculo do módulo das funções trigonométricas
+y_cos = np.abs(np.cos(x))
+y_sin = np.abs(np.sin(x))
