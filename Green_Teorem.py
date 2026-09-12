ef4671a068a4b7b4168e573882bb413e48334eb1
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-12 13:53:30 -0300

    4

--- Green_Teorem.py
+++ Green_Teorem.py
@@ -3,3 +3,8 @@ import sympy as sp
 def demonstrar_teorema_green():
     # 1. Definindo as variáveis simbólicas
     x, y = sp.symbols('x y')
+
+    # 2. Definindo o campo vetorial F = (L, M)
+    # Escolhemos um campo vetorial arbitrário para o teste
+    L = x**2 - y**2
+    M = 2*x*y
