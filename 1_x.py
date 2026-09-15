e79f29546ee20184a2e48c93960ed93384b89472
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-15 18:36:52 -0300

    2

--- 1_x.py
+++ 1_x.py
@@ -1,2 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Define os intervalos para x evitando o zero (onde a função não é definida)
+x_neg = np.linspace(-10, -0.1, 500)
+x_pos = np.linspace(0.1, 10, 500)
