97fbcf557af18992a085746394d9af71b2964b6d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-17 20:20:48 -0300

    5

--- coeficiente_angu.c
+++ coeficiente_angu.c
@@ -5,4 +5,21 @@ int main(){
 
     printf("--- Calculadora de Coeficiente Angular ---\n\n");
 
+    // Entrada dos dados do primeiro ponto
+    printf("Digite as coordenadas do Ponto A (x1 y1): ");
+    scanf("%f %f", &x1, &y1);
+
+    // Entrada dos dados do segundo ponto
+    printf("Digite as coordenadas do Ponto B (x2 y2): ");
+    scanf("%f %f", &x2, &y2);
+
+    // Verificação para evitar divisão por zero (reta vertical)
+    if (x1 == x2) {
+        printf("\nErro: A reta e vertical (x1 = x2). O coeficiente angular e indefinido.\n");
+    } else {
+        // Cálculo do coeficiente angular
+        m = (y2 - y1) / (x2 - x1);
+        printf("\nO coeficiente angular (m) da reta e: %.2f\n", m);
+    }
+
 }
