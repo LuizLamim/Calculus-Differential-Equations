9c8825e31daee356911161b67612fb5006c53300
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-21 21:17:54 -0300

    Dados de exemplo fictícios

--- torch_AI.py
+++ torch_AI.py
@@ -8,3 +8,6 @@ model = nn.Linear(1, 1)
 # Definindo a função de perda e o otimizador
 criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Dados de exemplo fictícios
+X = torch.tensor([[1.0], [2.0], [3.0]], dtype=torch.float32)
