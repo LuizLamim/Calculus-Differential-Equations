e0e8d5bdaa1bec5cb92002e33ca8d17d51dac26a
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-21 21:18:03 -0300

    2

--- torch_AI.py
+++ torch_AI.py
@@ -11,3 +11,4 @@ optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 # Dados de exemplo fictícios
 X = torch.tensor([[1.0], [2.0], [3.0]], dtype=torch.float32)
+y = torch.tensor([[2.0], [4.0], [6.0]], dtype=torch.float32)
