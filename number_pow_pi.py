1323e2ebba10228b1063af4e206fc7e8a86d2835
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-23 18:55:51 -0300

    5

--- number_pow_pi.py
+++ number_pow_pi.py
@@ -2,7 +2,7 @@ import math
 
 def calcular_potencia_pi():
     print("--- Calculadora de Número Elevado a Pi ---")
-
+    
     try:
         # Pede para o usuário digitar um número
         numero = float(input("Digite um número: "))
@@ -13,6 +13,9 @@ def calcular_potencia_pi():
         # Mostra o resultado formatado
         print(f"\n{numero} elevado a pi (aprox. {math.pi:.4f}) é igual a:")
         print(f"-> {resultado}")
-
-        except ValueError:
+        
+    except ValueError:
         print("Erro: Por favor, digite apenas números válidos.")
+
+if __name__ == "__main__":
+    calcular_potencia_pi()
