85ac98c48fe2a2fc802a4dea6d242b6930e5ecd6
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-23 18:55:43 -0300

    4

--- number_pow_pi.py
+++ number_pow_pi.py
@@ -13,3 +13,6 @@ def calcular_potencia_pi():
         # Mostra o resultado formatado
         print(f"\n{numero} elevado a pi (aprox. {math.pi:.4f}) é igual a:")
         print(f"-> {resultado}")
+
+        except ValueError:
+        print("Erro: Por favor, digite apenas números válidos.")
