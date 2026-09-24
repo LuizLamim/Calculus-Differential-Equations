723f3a72c5891069e55e025bc12487798cf0da64
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-24 19:13:10 -0300

    4

--- primo_pow_pi.py
+++ primo_pow_pi.py
@@ -5,3 +5,6 @@ primo = 3
 
 # Calculando primo elevado a pi (3 ** pi)
 resultado = primo ** math.pi
+
+# Exibindo o resultado formatado
+print(f"O número primo {primo} elevado a pi (π ≈ {math.pi:.5f}) é:")
