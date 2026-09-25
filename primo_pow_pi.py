8a1ce81991c05401c9aa0ed5e7b0c060c90d161e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-09-25 20:09:10 -0300

    23

--- primo_pow_pi.py
+++ primo_pow_pi.py
@@ -1,9 +1,9 @@
 import math
 
-# Definindo um número primo (ex: 17)
-primo = 17
+# Definindo um número primo (ex: 23)
+primo = 23
 
-# Calculando primo elevado a pi (17 ** pi)
+# Calculando primo elevado a pi (23 ** pi)
 resultado = primo ** math.pi
 
 # Exibindo o resultado formatado
